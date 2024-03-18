11ccacb484a64339eaae7eb110252e106d967f1f
CATOZOLALA <91690955+CATOZOLALA@users.noreply.github.com>
2024-03-18 22:16:27 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'catozolala'// yourname
+global.MONGO_DB_URI = "mongodb+srv://catozolala:ft2iPJmcKvpAddYn@cluster0.i4oxhvt.mongodb.net/" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "catozolala@gmail.com" //email
+global.email_password = "lalabulbul#3" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
